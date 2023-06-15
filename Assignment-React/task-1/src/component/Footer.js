5af51d6622954e69ca587efc9f0a53ea11b34a61
Mayank Kumar Patel <72437985+night05fury@users.noreply.github.com>
2023-06-15 18:26:40 +0530

    Created components for the Navigation bar, text area and Footer

---
+++ Assignment-React/task-1/src/component/Footer.js
@@ -0,0 +1,9 @@
+// This is Footer component of the Word Count 
+
+export default function Footer(){
+    return (
+        <footer id="footer" className="absolute ">
+            Made by Mayank Kumar Patel
+        </footer>
+    )
+}
