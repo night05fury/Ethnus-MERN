8dbe930b1479604ab47e639abadcecb2389962db
Mayank Kumar Patel <72437985+night05fury@users.noreply.github.com>
2023-06-15 20:22:51 +0530

    Completed the NavBar and their elements

--- Assignment-React/task-1/src/App.js
+++ Assignment-React/task-1/src/App.js
@@ -1,17 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-import Footer from './component/Footer';
+import React from 'react';
+
 import Navbar from './component/Navbar';
+import TextArea from './component/TextArea';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
 
 function App() {
   return (
     <>
-    
-    <Navbar />
-    <Footer />
+      <Router>
+        <Navbar tittle="Word Counter" />
+
+        <div className="container my-3">
+          <Routes>
+           
+            <Route path="/" element={ <TextArea heading="Enter the text to analyze" />}>
+             
+            </Route>
+          </Routes>
+        </div>
+        
+      </Router>
     </>
-   
-  );
+  )
 }
 
-export default App;
+export default App
--- Assignment-React/task-1/src/component/Footer.js
+++
@@ -1,9 +0,0 @@
-// This is Footer component of the Word Count 
-
-export default function Footer(){
-    return (
-        <footer id="footer" className="absolute ">
-            Made by Mayank Kumar Patel
-        </footer>
-    )
-}
